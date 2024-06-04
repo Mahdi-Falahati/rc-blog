a830d2b71a4d9c3a454fb105d126b989b3d03272
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-04 01:33:25 -0700

    write base tag and some style to theme toggle

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
 import styles from "./themeToggle.module.css";
 
 export default function ThemeToggle() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="" height={14} width={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="" height={14} width={14} />
+    </div>
+  );
 }
