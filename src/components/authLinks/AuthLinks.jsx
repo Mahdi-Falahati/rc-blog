a80e50115a156d46ac41dfcac344d2e713aa21ef
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-04 01:12:14 -0700

    write som logic for link user to login and logout

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,5 +1,17 @@
-import styles from "./authLinks.module.css";
+import Link from "next/link";
 
 export default function AuthLinks() {
-  return <div></div>;
+  const status = "notauthenticated";
+  return (
+    <>
+      {status === "notauthenticated" ? (
+        <Link href="/login">Login</Link>
+      ) : (
+        <>
+          <Link href="/write">Write</Link>
+          <span style={{ cursor: "pointer" }}>Logout</span>
+        </>
+      )}
+    </>
+  );
 }
