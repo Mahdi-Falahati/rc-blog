e22f7ee9db1446aeeafa177f109531c4431da0aa
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-04 03:07:15 -0700

    make burger menu and completed responsive navbar

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,16 +1,48 @@
+"use client";
+
+import styles from "./authLinks.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function AuthLinks() {
+  const [open, setOpen] = useState(false);
   const status = "notauthenticated";
+
   return (
     <>
       {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-          <Link href="/write">Write</Link>
+          <Link href="/write" className={styles.link}>
+            Write
+          </Link>
           <span style={{ cursor: "pointer" }}>Logout</span>
         </>
+      )}
+
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span style={{ cursor: "pointer" }}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
