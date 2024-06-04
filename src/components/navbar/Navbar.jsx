7a35f7b61e078b13482e7149df207cc8dd4a49d6
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-04 01:04:04 -0700

    some style to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,14 +6,14 @@ import AuthLinks from "../authLinks/AuthLinks";
 
 export default function Navbar() {
   return (
-    <div className={styles.containary}>
+    <div className={styles.container}>
       <div className={styles.social}>
         <Image src="/facebook.png" alt="facebook" width={24} height={24} />
         <Image src="/instagram.png" alt="instagram" width={24} height={24} />
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>MahdiFalahati-Blog</div>
+      <div className={styles.logo}>RC</div>
       <div className={styles.links}>
         <ThemeToggle />
         <Link href="/">Home</Link>
