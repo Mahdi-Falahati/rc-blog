ccc4bc2cdc9cc839701423a7fb0a9dd0a235a694
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-04 00:37:55 -0700

    add socials media icon to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
 import styles from "./navbar.module.css";
 
 export default function Navbar() {
-  return <div> navbar </div>;
+  return (
+    <div className={styles.containary}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+      </div>
+    </div>
+  );
 }
