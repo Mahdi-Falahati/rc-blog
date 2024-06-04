ede882b787890322ee3cf89e7bdbaf3dd5b55741
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-04 03:10:32 -0700

    fixed error

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,7 +5,7 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
-  if (typeof window !== undefined) {
+  if (typeof window !== "undefined") {
     const value = localStorage.getItem("theme");
     return value || "light";
   }
