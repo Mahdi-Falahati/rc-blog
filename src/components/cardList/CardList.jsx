ebdeb162c5fd0b9853669a99fc0ab058cf3289b0
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-05 04:23:34 -0700

    use card component to cardList

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,10 +1,17 @@
+import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
 import styles from "./cardList.module.css";
 
 export default function CardList() {
   return (
-    <div>
-      cardList
+    <div className={styles.container}>
+      <h2 className={styles.title}>Recent Posts</h2>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Pagination />
     </div>
   );
