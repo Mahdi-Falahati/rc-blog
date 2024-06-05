d6d7ab526e843f0db18a8816cc1dc488bfa76de9
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-05 03:08:35 -0700

    add routes and buttons to categories so copmlete category component

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,5 +1,73 @@
+import Link from "next/link";
 import styles from "./categoryList.module.css";
+import Image from "next/image";
 
 export default function CategoryList() {
-  return <div>categoryList</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Popular Categories</h2>
+      <div className={styles.categories}>
+        <Link href={`/blog`} className={`${styles.category} ${styles.style}`}>
+          <Image
+            src={"/style.png"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.fashion}`}>
+          <Image
+            src={"/fashion.png"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          fashion
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.food}`}>
+          <Image
+            src={"/food.png"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          food
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.travel}`}>
+          <Image
+            src={"/travel.png"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          travel
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.culture}`}>
+          <Image
+            src={"/culture.png"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          culture
+        </Link>
+        <Link href={`/blog`} className={`${styles.category} ${styles.coding}`}>
+          <Image
+            src={"/coding.png"}
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          coding
+        </Link>
+      </div>
+    </div>
+  );
 }
