c46d3fb7b2e0f617f9522a16c40e8df17a545caf
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-06 23:55:49 -0700

    complete third part of side menu ( category List )

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -68,6 +68,44 @@ export default function Menu() {
         </Link>
       </div>
 
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <section className={styles.categoryList}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.categoryItem} ${styles.style}`}
+        >
+          Style
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.fashion}`}
+        >
+          Fashion
+        </Link>
+        <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>
+          Food
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.travel}`}
+        >
+          Travel
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.culture}`}
+        >
+          Culture
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.coding}`}
+        >
+          Coding
+        </Link>
+      </section>
+
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
       <div className={styles.items}>
