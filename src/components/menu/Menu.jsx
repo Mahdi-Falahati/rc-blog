240880920ff97d26fa56a3b4f319540e8e12e3e9
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-06 23:47:49 -0700

    coplete the secound part of side menu ( Editors Pick )

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -9,6 +9,69 @@ export default function Menu() {
       <h1 className={styles.title}>Most Popular</h1>
       <div className={styles.items}>
         <Link href="/" className={styles.item}>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              Non exercitation veniam consequat dolor.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Mahdi Falahati</span>
+              <span className={styles.dash}>-</span>
+              <span className={styles.date}>10.03.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>
+              Culture
+            </span>
+            <h3 className={styles.postTitle}>
+              Non exercitation veniam consequat dolor.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Mahdi Falahati</span>
+              <span className={styles.dash}>-</span>
+              <span className={styles.date}>10.03.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            <h3 className={styles.postTitle}>
+              Non exercitation veniam consequat dolor.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Mahdi Falahati</span>
+              <span className={styles.dash}>-</span>
+              <span className={styles.date}>10.03.2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>
+              Fashion
+            </span>
+            <h3 className={styles.postTitle}>
+              Non exercitation veniam consequat dolor.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Mahdi Falahati</span>
+              <span className={styles.dash}>-</span>
+              <span className={styles.date}>10.03.2024</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
           <div className={styles.imageContainer}>
             <Image
               src="/p1.jpeg"
