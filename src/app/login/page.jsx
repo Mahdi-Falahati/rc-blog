720a7df45060fb8592df4d7d7abe886d1032a34b
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-08 07:50:27 -0700

    add conditions for loading and do usr to home page after login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,27 @@
-import styles from "./login.module.css";
+"use client";
 
-export default function page() {
+import { signIn, useSession } from "next-auth/react";
+import styles from "./login.module.css";
+import { useRouter } from "next/router";
+
+export default function LoginPage() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sign in with Google</div>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
+        </div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
