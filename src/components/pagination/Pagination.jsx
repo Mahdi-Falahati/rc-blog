9e32e0ec66e86b1a3898972213c2cc5f4268a8a7
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-10 05:54:34 -0700

    complete pagination component so it can change page and ...

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,10 +1,30 @@
-import styles from "./pagination.module.css";
+"use client";
 
-export default function Pagination() {
+import React from "react";
+import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
+
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
-      <button className={styles.button}>Previous</button>
-      <button className={styles.button}>Next</button>
+      <button
+        className={styles.button}
+        disabled={!hasPrev}
+        onClick={() => router.push(`?page=${page - 1}`)}
+      >
+        Previous
+      </button>
+      <button
+        disabled={!hasNext}
+        className={styles.button}
+        onClick={() => router.push(`?page=${page + 1}`)}
+      >
+        Next
+      </button>
     </div>
   );
-}
+};
+
+export default Pagination;
