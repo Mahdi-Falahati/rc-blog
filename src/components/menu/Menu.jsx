7e6be3b809f1cb235406510111fc41c2c8754635
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-10 11:47:28 -0700

    remove nessery import

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "./menu.module.css";
-import Link from "next/link";
-import Image from "next/image";
 import MenuPosts from "../menuPosts/MenuPosts";
 import MenuCategories from "../menuCategories/MenuCategories";
 
