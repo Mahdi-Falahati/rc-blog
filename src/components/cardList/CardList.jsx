278ecacda5ea7cd0b17252501a05b624a804842a
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-10 05:27:28 -0700

    get  data from api and show its

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,18 +1,39 @@
-import Card from "../card/Card";
-import Pagination from "../pagination/Pagination";
+import React from "react";
 import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+import Card from "../card/Card";
 
-export default function CardList() {
+const getData = async (page, cat) => {
+  const res = await fetch(`http://localhost:3000/api/posts`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CardList = async ({ page, cat }) => {
+  const { posts, count } = await getData(page, cat);
+
+  const POST_PER_PAGE = 2;
+
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Recent Posts</h2>
+      <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {posts?.map((item) => (
+          <Card item={item} key={item._id} />
+        ))}
       </div>
-      <Pagination />
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
-}
+};
+
+export default CardList;
