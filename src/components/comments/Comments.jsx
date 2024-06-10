6d0ebed98cc1a6360936ff37f6c619dc925c3ee5
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-10 06:52:11 -0700

    use data and complete comment component

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,44 +1,86 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./comments.module.css";
 import Image from "next/image";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
-export default function Comments() {
-  const status = "authenticated";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message);
+    throw error;
+  }
+
+  return data;
+};
+
+const Comments = ({ postSlug }) => {
+  const { status } = useSession();
+
+  const { data, mutate, isLoading } = useSWR(
+    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    fetcher
+  );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    mutate();
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
-          <textarea placeholder="Write a comment..." className={styles.input} />
-          <button className={styles.button}>Send</button>
+          <textarea
+            placeholder="write a comment..."
+            className={styles.input}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       ) : (
-        <Link href="/login">Loging to write comments</Link>
+        <Link href="/login">Login to write a comment</Link>
       )}
-
       <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src="/p1.jpeg"
-              width={50}
-              height={50}
-              alt=""
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>Mahdi Falahati</span>
-              <span className={styles.date}>10.10.2024</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Aliquip esse dolor cillum qui ea occaecat. Ullamco deserunt id
-            occaecat anim exercitation non. Occaecat ut id veniam aliquip
-            incididunt exercitation veniam nostrud eiusmod excepteur pariatur
-            exercitation. Laborum ut aliqua ex aliqua anim occaecat.
-          </p>
-        </div>
+        {isLoading
+          ? "loading"
+          : data?.map((item) => (
+              <div className={styles.comment} key={item._id}>
+                <div className={styles.user}>
+                  {item?.user?.image && (
+                    <Image
+                      src={item.user.image}
+                      alt=""
+                      width={50}
+                      height={50}
+                      className={styles.image}
+                    />
+                  )}
+                  <div className={styles.userInfo}>
+                    <span className={styles.username}>{item.user.name}</span>
+                    <span className={styles.date}>{item.createdAt}</span>
+                  </div>
+                </div>
+                <p className={styles.desc}>{item.desc}</p>
+              </div>
+            ))}
       </div>
     </div>
   );
-}
+};
+
+export default Comments;
