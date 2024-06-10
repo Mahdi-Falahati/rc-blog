9c796382587bbfefefc17aeb856d6420c8d74d96
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-10 06:04:38 -0700

    add search param to blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,15 +1,20 @@
 import CardList from "@/components/cardList/CardList";
-import Menu from "@/components/menu/Menu";
 import styles from "./blogPage.module.css";
+import Menu from "@/components/Menu/Menu";
 
-export default function BlogPage() {
+const BlogPage = ({ searchParams }) => {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}> Style Blog</h1>
+      <h1 className={styles.title}>{cat} Blog</h1>
       <div className={styles.content}>
-        <CardList />
+        <CardList page={page} cat={cat} />
         <Menu />
       </div>
     </div>
   );
-}
+};
+
+export default BlogPage;
