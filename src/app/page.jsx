00f615331fee929b28281b8410eb74d270f44560
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-10 05:29:13 -0700

    get search params in home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,9 @@ import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
-export default function Home() {
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
   return (
     <div className={styles.container}>
       <Featured />
