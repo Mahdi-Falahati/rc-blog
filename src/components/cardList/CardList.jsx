4bcb468624b18bdf3aba77a3e11304cf4cd01e01
Mahdi-Falahati <falahatimahdi01@gmail.com>
2024-06-10 06:09:18 -0700

    add cat to card list and complete this component

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
+import Image from "next/image";
 import Card from "../card/Card";
 
 const getData = async (page, cat) => {
-  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     throw new Error("Failed");
